6389a42a9ad23f268db32bcd23c457c6758649d2
monkafrog <saphal1998@gmail.com>
2024-09-13 21:07:03 -0400

    PRIME: 0 indeed not prime

--- prime/prime.main.go
+++ prime/prime.main.go
@@ -82,10 +82,6 @@ func handleConnection(conn net.Conn) {
 func checkPrime(f float64) bool {
 	number := int(f)
 
-	if number == 0 {
-		return true
-	}
-
 	if number <= 1 {
 		return false
 	}
@@ -99,7 +95,7 @@ func checkPrime(f float64) bool {
 	}
 
 	limit := int(math.Floor(math.Sqrt(float64(number))))
-	for i := 3; i <= limit; i += 1 {
+	for i := 3; i <= limit; i += 2 {
 		if number%i == 0 {
 			return false
 		}
